73c128a980f57a46dfbb774f05b2dbc538ef9f47
vipulchakravarthy <saivipul15@msitprogram.net>
2021-06-12 16:57:22 +0530

    completed topic servie taksk

--- demo/src/main/java/com/example/demo/TopicController.java
+++ demo/src/main/java/com/example/demo/TopicController.java
@@ -3,16 +3,18 @@ package com.example.demo;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getTopics() {
-        return Arrays.asList(new Topic("124", "Spring course", "This is my description"),
-                new Topic("126", "React", "This is my description"),
-                new Topic("128", "Java", "This is my description"));
+        return topicService.getTopics();
     }
 }
